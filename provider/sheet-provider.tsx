45f988d9bd5eca8f595abce16d52acc3b4f953eb
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-07-20 12:23:25 +0530

    new

--- provider/sheet-provider.tsx
+++ provider/sheet-provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { EditAccountSheet } from "@/features/accounts/components/edit-account-sheet";
 import { NewAccountSheet } from "@/features/accounts/components/new-account-sheet";
 import { useMountedState } from "react-use";
 
@@ -14,6 +15,7 @@ export const SheetProvider = () => {
     return (
         <>
             <NewAccountSheet/>
+            <EditAccountSheet/>
         </>
     )
 }
