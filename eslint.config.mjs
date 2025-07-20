211f3ee1ec7dc1f69e24648b6cb53b90fbce1c72
Vishal Gaikwad <gaikwad8vishal@gmail.com>
2025-07-21 01:01:05 +0530

    new

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  {
+    ignores: [
+      ".next/**",
+      "node_modules/**",
+      "lib/generated/prisma/**", // ✅ fully ignore generated Prisma files
+    ],
+  },
 ];
 
 export default eslintConfig;
